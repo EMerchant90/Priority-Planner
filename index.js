907f3fd21be6cb3573d90f02069c032db8c5fa89
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-08-01 13:30:00 -0400

    adding node and express to new index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express.js";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
