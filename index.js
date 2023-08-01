3fe1672e6d55ee993811322e2d1d7a82ed152747
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-08-01 13:42:47 -0400

    checking for right index.js file

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/work", (req, res) => {
+    res.send("Hello, About!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+})
