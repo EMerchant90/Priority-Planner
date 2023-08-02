3eea552df11bed926c88e01fbed5adcb8f790b3c
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-08-02 19:10:03 -0400

    Added strikethrough of tasks on original and /work page as well

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var taskList = [];
+var workTaskList = [];
  
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { task: taskList});
 });
 
 app.get("/work", (req, res) => {
-    res.send("Hello, Work!");
+    res.render("work.ejs", { task: workTaskList });
 });
 
 app.post("/submit", (req, res) => {
-    const newTask = req.body["task"];
-    console.log(newTask);
-    res.render("index.ejs", { task: newTask });
+    taskList.push(req.body["task"]);
+    console.log(taskList);
+    res.render("index.ejs", { task: taskList });
 })
+
+app.post("/work", (req, res) => {
+    workTaskList.push(req.body["task"]);
+    console.log(workTaskList);
+    res.render("work.ejs", { task: workTaskList });
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
