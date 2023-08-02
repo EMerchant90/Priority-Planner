b6657cb9044a0fdcb1468902fc7e207da7555f16
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-08-02 10:22:23 -0400

    Added ejs functionality to take task and add it temp to page

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-import express from "express.js";
+import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+ 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.render("index.ejs");
 });
 
 app.get("/work", (req, res) => {
-    res.send("Hello, About!");
+    res.send("Hello, Work!");
 });
+
+app.post("/submit", (req, res) => {
+    const newTask = req.body["task"];
+    console.log(newTask);
+    res.render("index.ejs", { task: newTask });
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 })
+
